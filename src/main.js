f2d845333bafa26f0d77f8bfd61ad9367756da90
Pasha0923 <alexsai@i.ua>
2024-01-21 23:25:06 +0300

    add change homework11

--- src/main.js
+++ src/main.js
@@ -9,33 +9,38 @@ const API_KEY = '41870399-9b44301246ceb98c07efd626a';
 const refs = {
   searchForm: document.querySelector('.search-form'),
   photoList: document.querySelector('.photo-list'),
+  loader: document.querySelector('.loader'),
 };
-
+refs.loader.style.display = 'none';
 refs.searchForm.addEventListener('submit', handleSearch); // обробник подій на форму
+
 function handleSearch(event) {
-  // функція яка описує обробник події
-  event.preventDefault();
+  event.preventDefault(); // скидуємо стандартну поведінку браузера
+  // функція,яка описує обробник події  handleSearch
+  refs.loader.style.display = 'inline-block';
+  refs.photoList.innerHTML = ''; // очищаємо вміст галереї (перед здійсненням нового пошуку вміст
+  // галереї очищаємо щоб уникнути переплутання результатів)
   const form = event.currentTarget;
   const query = form.elements.query.value; // дістаємо значення з форми
+  // перевірка на пустий рядок запиту
+  if (query === '') {
+    iziToast.show({
+      message: 'Please enter your request',
+      position: 'topRight',
+      color: 'yellow',
+    });
+    refs.loader.style.display = 'none';
 
+    return;
+  }
   searchPhoto(query)
     .then(createMarkuPhoto)
-    .catch(onFetchError)
+    .catch(err => console.log(err))
     .finally(() => form.reset());
-
-  searchPhoto(query).then(data => {
-    if (!data.hits.length || query === '') {
-      iziToast.error({
-        title: 'Error',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-      });
-      return;
-    }
-  });
+  console.log(query);
 }
 
-// пишемо функцію для того щоб робити сам запит
+// пишемо функцію для того щоб робити запит
 function searchPhoto(value) {
   return fetch(
     `${BAZE_URL}/?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=9`
@@ -43,6 +48,7 @@ function searchPhoto(value) {
     if (!resp.ok) {
       throw new Error(resp.statusText);
     }
+
     return resp.json();
   });
 }
@@ -56,11 +62,20 @@ function searchPhoto(value) {
 //     safesearch: true,
 //   });
 // }
-function onFetchError(error) {
-  console.error(error);
-}
 
 function createMarkuPhoto({ hits }) {
+  refs.loader.style.display = 'none';
+  // перевірка на відсутність результатів від бекенду (якщо бекенд повертає порожній масив)
+  if (hits.length === 0) {
+    iziToast.error({
+      title: 'Error',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+    });
+    console.log(hits);
+    return;
+  }
+  // створення розмітки
   const markup = hits
     .map(
       hits => `<li class="gallery-item">
@@ -77,8 +92,9 @@ function createMarkuPhoto({ hits }) {
 </li>`
     )
     .join('');
-
+  // додаємо нові елементи до галереї
   refs.photoList.innerHTML = markup;
+  // викликали метод бібліотеки SimpleLightbox
   refreshPage.refresh();
 }
 
